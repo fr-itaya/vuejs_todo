55d3bae6ce3b77e048d9541a324ce18ab89df3af
fr-itaya <itaya@fact-real.com>
2014-10-31 16:55:49 +0900

    新規登録画面テンプレート作成・Vue.jsで新規登録画面のコンポーネント作成 #1

--- main.js
+++ main.js
@@ -1,39 +1,56 @@
 (function() {
-// TODO: Access-Control-Allow-origin対策してからコメント外す
-//var milkcocoa = new MilkCocoa("http://io-wi1roseru.mlkcca.com");
+  // TODO: Access-Control-Allow-origin対策してからコメント外す
+  //var milkcocoa = new MilkCocoa("http://io-wi1roseru.mlkcca.com");
 
-var app = new Vue({
-  el: "#content",
-  data: {
-    currentView: null
-  }
-});
-
-app.currentView = "todo";
-
-Vue.component('todo', {
-  template: "#todo-list-template",
-  data: {
-    title: "今日のTODOs",
-    //model
-    todos: []
-  },
-  methods: {
-    add:function() {
-      this.todos.push({
-        content: this.todoText
-      });
-      this.todoText = "";
-    },
-
-    delete:function(event) {
-      this.todos.$remove(event.targetVM.$data);
-    },
-
-    deleteAll:function() {
-      this.todos = [];
+  var app = new Vue({
+    el: "#content",
+    data: {
+      currentView: null
     }
-  }
-});
+  });
 
+  app.currentView = 'register';
+
+  Vue.component('todo', {
+    template: "#todo-list-template",
+    data: {
+      title: "今日のTODOs",
+      //model
+      todos: []
+    },
+    methods: {
+      add:function() {
+        this.todos.push({
+          content: this.todoText
+        });
+        this.todoText = "";
+      },
+
+      delete:function(event) {
+        this.todos.$remove(event.targetVM.$data);
+      },
+
+      deleteAll:function() {
+        this.todos = [];
+      }
+    }
+  });
+
+  Vue.component('register', {
+    template: "#register-template",
+    data: {
+      email: "",
+      password: "",
+      confirm: ""
+    },
+    methods: {
+      register: function() {
+        var self = this;
+        if(this.password != this.confirm) return;
+      },
+      goto_login_view: function() {
+        app.currentView = "login";
+      }
+    }
+  });
 })();
